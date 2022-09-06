38f2b682610cfbd7a3355d08926c99fcf758a955
DoggyTY <ctcriechert@gmail.com>
2022-09-06 15:06:09 +0700

    Converted files to C++
    Just convert python files to cpp files

---
+++ Project files/RobotTest/src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       e2622588                                                  */
+/*    Created:      Tue Sep 06 2022                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}

415d48c4fae5eabd4a5ed1dfd7f10e9baeae53aa
aRaincloud <112934241+aRaincloud@users.noreply.github.com>
2022-09-27 15:56:57 +0700

    Update main.cpp

--- Project files/RobotTest/src/main.cpp
+++ Project files/RobotTest/src/main.cpp
@@ -20,90 +20,124 @@
 #include "vex.h"
 
 using namespace vex;
-int GoalY = 0;
-int GoalX = 0;
-int GoalZ = 0;
-double RobotY = 0;
-double RobotX = 0; // Need to change this to GPS sensors
-double RobotZ = 0;
-double RobotHead = 0;
-// Right Joystick Horizonal
-int RightSide = Controller1.Axis1.position(percent);
-// Right Joystick Vertical
-int RightVert = Controller1.Axis2.position(percent);
-// Left Joystick Vertical
-int LeftVert = Controller1.Axis3.position(percent);
-// Left Joystick Horizonal
-int LeftSide = Controller1.Axis4.position(percent);
+class Test { 
+  bool Auto = false;
+  int GoalY = 0;
+  int GoalX = 0;
+  int GoalZ = 0;
+  double RobotY = 0;
+  double RobotX = 0; // Need to change this to GPS sensors
+  double RobotZ = 0;
+  double RobotHead = 0;
+  // Right Joystick Horizonal
+  int RightSide = Controller1.Axis1.position(percent);
+  // Right Joystick Vertical
+  int RightVert = Controller1.Axis2.position(percent);
+  // Left Joystick Vertical
+  int LeftVert = Controller1.Axis3.position(percent);
+  // Left Joystick Horizonal
+  int LeftSide = Controller1.Axis4.position(percent);
 
-int i = 215;
-int main() {
-  // Initializing Robot Configuration. DO NOT REMOVE!
-  vexcodeInit();
-  AceBase();
-  //wait(4,sec);
-  //Player();
-}
-
-double FindGoal(int x, int z){
-  return sqrt(pow(abs(GoalX-x),2)+pow(abs(z-GoalZ),2));
-}
-
-
-// Player control
-void Player() {
-  LeftDrive.setVelocity(LeftVert,pct);
-  LeftDriveMotorC.setVelocity(LeftVert,pct);
-  RightDrive.setVelocity(LeftVert,pct);
-  RightDriveMotorC.setVelocity(LeftVert,pct);
-  while (true){
-    //LeftDrive, Left wheels -- LeftDriveMotorB
-    //RightDrive, Right wheels -- RightDriveMotorE
-    LeftDrive.spin(forward);
-    LeftDriveMotorC.spin(forward);
-    RightDrive.spin(forward);
-    RightDriveMotorC.spin(forward);
-    if (LeftSide > 33 or LeftSide < -33) {
-      LeftDrive.setVelocity(LeftVert+LeftSide,pct);
-      LeftDriveMotorC.setVelocity(LeftVert+LeftSide,pct);
-      RightDrive.setVelocity(LeftVert-LeftSide,pct);
-      RightDriveMotorC.setVelocity(LeftVert-LeftSide,pct);
-    } else {
-      LeftDrive.setVelocity(LeftVert,pct);
-      LeftDriveMotorC.setVelocity(LeftVert,pct);
-      RightDrive.setVelocity(LeftVert,pct);
-      RightDriveMotorC.setVelocity(LeftVert,pct);
-    }
+  int i = 215;
+  int main() {
+    // Initializing Robot Configuration. DO NOT REMOVE!
+    vexcodeInit();
+    AceBase();
+    AceBaseAuto();
+    //wait(4,sec);
+    //Player();
+    return 0;
   }
-}
 
-void AceBase(){
-  while (true){
-    if (Controller1.ButtonUp.pressing()){
+  double FindGoal(int x, int z){
+    return sqrt(pow(abs(GoalX-x),2)+pow(abs(z-GoalZ),2));
+  }
+
+
+  // Player control
+  void Player() {
+    LeftDrive.setVelocity(LeftVert,pct);
+    LeftDriveMotorC.setVelocity(LeftVert,pct);
+    RightDrive.setVelocity(LeftVert,pct);
+    RightDriveMotorC.setVelocity(LeftVert,pct);
+    while (true){
+      //LeftDrive, Left wheels -- LeftDriveMotorB
+      //RightDrive, Right wheels -- RightDriveMotorE
       LeftDrive.spin(forward);
-      RightDrive.spin(reverse);
-      LeftDriveMotorC.spin(reverse);
-      RightDriveMotorC.spin(forward);
-    }
-    else if (Controller1.ButtonRight.pressing()) {
-      LeftDrive.spin(reverse);
-      RightDrive.spin(forward);
       LeftDriveMotorC.spin(forward);
-      RightDriveMotorC.spin(reverse);
-    }
-    else if (Controller1.ButtonLeft.pressing()){
-      LeftDrive.spin(forward);
-      RightDrive.spin(reverse);
-      LeftDriveMotorC.spin(reverse);
+      RightDrive.spin(forward);
       RightDriveMotorC.spin(forward);
-    }
-    else {
-      LeftDrive.stop();
-      RightDrive.stop();
-      LeftDriveMotorC.stop();
-      RightDriveMotorC.stop();
+      if (LeftSide > 33 or LeftSide < -33) {
+        LeftDrive.setVelocity(LeftVert+LeftSide,pct);
+        LeftDriveMotorC.setVelocity(LeftVert+LeftSide,pct);
+        RightDrive.setVelocity(LeftVert-LeftSide,pct);
+        RightDriveMotorC.setVelocity(LeftVert-LeftSide,pct);
+      } else {
+        LeftDrive.setVelocity(LeftVert,pct);
+        LeftDriveMotorC.setVelocity(LeftVert,pct);
+        RightDrive.setVelocity(LeftVert,pct);
+        RightDriveMotorC.setVelocity(LeftVert,pct);
+      }
     }
   }
-}
+
+  void AceBase(){
+    while (!Auto){
+      if (Controller1.ButtonUp.pressing()){
+        LeftDrive.spin(forward);
+        RightDrive.spin(reverse);
+      }
+      else if (Controller1.ButtonRight.pressing()) {
+        LeftDrive.spin(reverse);
+        RightDrive.spin(forward);
+      }
+      else if (Controller1.ButtonLeft.pressing()){
+        LeftDrive.spin(forward);
+        RightDrive.spin(reverse);
+      }
+      else if (Controller1.ButtonA.pressing()){
+        Auto = true;
+      }
+      else {
+        LeftDrive.stop();
+        RightDrive.stop();
+      }
+    }
+  }
+
+
+  void AceBaseAuto() {
+    while (Auto) {
+      LeftDrive.spinFor(forward,1,turns,false);
+      RightDrive.spinFor(forward,1,turns);
+      //we need to find out how far 1 turn of the mortor is or what the 
+      //method for the robot to drive a fixed distance because I forget what that is
+      LeftDrive.spinFor(reverse,1,turns, false);
+      RightDrive.spinFor(forward,1,turns);
+      //first turn
+      LeftDrive.spinFor(forward,1,turns,false);
+      RightDrive.spinFor(forward,1,turns);
+      //second straight portion
+      LeftDrive.spinFor(forward,1,turns, false);
+      RightDrive.spinFor(reverse,1,turns);
+      //second turn
+      LeftDrive.spinFor(forward,1,turns,false);
+      RightDrive.spinFor(forward,1,turns);
+      //third straight portion
+      LeftDrive.spinFor(forward,1,turns, false);
+      RightDrive.spinFor(reverse,1,turns);
+      //third turn
+      LeftDrive.spinFor(forward,1,turns,false);
+      RightDrive.spinFor(forward,1,turns);
+      //fourth straight portion
+      LeftDrive.spinFor(reverse,1,turns, false);
+      RightDrive.spinFor(forward,1,turns);
+      //fifth turn
+      LeftDrive.spinFor(forward,1,turns,false);
+      RightDrive.spinFor(forward,1,turns);
+      //fifth straight portion
+    }
+  }
+};
 
 // Don't look down here there isn't anything down here but suffering :)
